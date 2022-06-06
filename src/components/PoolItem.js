da89769576fb2d92a79cccc97893fb725ca72cb4
samantha <sasalinero@gmail.com>
2022-06-06 20:23:55 +0200

    select pool

--- src/components/PoolItem.js
+++ src/components/PoolItem.js
@@ -6,6 +6,7 @@ function PoolItem(props) {
       <p className="where_p_pool">Código Postal</p><p className="res_p_pool"> {props.poolData.postcode}</p>
       <p className="name_p_pool"> {props.poolData.title}</p>
       <p> {props.poolData.llegar}</p>
+      
       <a  href={props.poolData.horario} ><button className="btn_info" >Horario</button></a>
       
     </>
--- src/components/PoolList.js
+++ src/components/PoolList.js
@@ -4,30 +4,54 @@ import { useEffect, useState } from "react";
 import PoolItem from "./PoolItem";
 import "../styles/PoolList.scss";
 function PoolList() {
-  const [dataPool, setdataPool] = useState([]);
+  const [allPools, setAllPools] = useState([]);
+  const [searchPostCode, setSearchPostCode] = useState();
 
   useEffect((ev) => {
     getPoolFromApi().then((allPoolData) => {
-      setdataPool(allPoolData);
+      setAllPools(allPoolData);
     });
   }, []);
-  console.log(dataPool)
 
-  const renderLiPool = dataPool.map((itemPool, index) => (
-    <li key={index} className="pool_li">
-      <PoolItem poolData={itemPool} />
-    </li>
-  ));
+  const handleChange = (ev) => {
+    setSearchPostCode(ev.currentTarget.value);
+  };
+
+  const renderLiPool = allPools
+    .filter((onePool) => searchPostCode === "Todos" || onePool.id === searchPostCode
+    )
+    .map((onePool, index) => (
+      <li key={index} className="pool_li">
+        <PoolItem poolData={onePool} />
+      </li>
+    ));
+
+  const renderOptionsPostcode = allPools.map((onePool, index) => {
+    return (
+      <option value={onePool.id} key={onePool.id}>
+        {onePool.postcode}
+      </option>
+    );
+  });
 
   return (
-  
     <div className="principal_pool">
-        <Header />
+      <Header />
+      <label htmlFor="postal">Código Postal</label>
+      <select
+        name="postcode"
+        id={searchPostCode}
+        value={searchPostCode}
+        onChange={handleChange}
+      >
+        <option value="Todos" key="">Todos</option>
+        {renderOptionsPostcode}
+      </select>
 
-      <ul className="pool_ul">{renderLiPool}</ul>
+      <ul className="pool_filtered">
+       {renderLiPool}
+      </ul>
     </div>
-    
-    
   );
 }
 export default PoolList;
